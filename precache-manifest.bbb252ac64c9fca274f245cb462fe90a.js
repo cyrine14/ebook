a89fb4db7df9542783d4f9ba98cc42658fbce684
cyrine14 <cyrinekarray14@gmail.com>
2020-09-02 12:51:50 +0200

    Updates

---
+++ precache-manifest.bbb252ac64c9fca274f245cb462fe90a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48790015fc7b1392ac666bcba397e52f",
+    "url": "/cyrine14/ebook.git/index.html"
+  },
+  {
+    "revision": "58fbb6ffa3803bda702f",
+    "url": "/cyrine14/ebook.git/static/css/2.428f4046.chunk.css"
+  },
+  {
+    "revision": "d75221871b353faec947",
+    "url": "/cyrine14/ebook.git/static/css/main.51f2bcd0.chunk.css"
+  },
+  {
+    "revision": "58fbb6ffa3803bda702f",
+    "url": "/cyrine14/ebook.git/static/js/2.5d5d703c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cyrine14/ebook.git/static/js/2.5d5d703c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75221871b353faec947",
+    "url": "/cyrine14/ebook.git/static/js/main.5b60add9.chunk.js"
+  },
+  {
+    "revision": "8a95f77f390a1ba6517e",
+    "url": "/cyrine14/ebook.git/static/js/runtime-main.6bcd617c.js"
+  }
+]);
